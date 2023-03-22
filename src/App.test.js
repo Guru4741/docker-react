8b5c21f425b8814fb40fb2c4076f86764df6d39f
Gaurav <gaurav4741@gmail.com>
2023-03-22 19:00:18 +0530

    changes test description

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello world link', () => {
+test('renders hello world', () => {
   render(<App />);
   const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
